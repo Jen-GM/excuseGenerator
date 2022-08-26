c712f0555890b4430c1cadffdb31f54a07e0873e
Jen-GM <jen.gonzalez.mu@gmail.com>
2022-08-26 01:37:36 +0000

    creacionejercicio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#refresher").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = createExcuse();
+  });
+};
+
+let createExcuse = () => {
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
+  let what = ["ate", "pissed", "used", "broked"];
+  let when = [
+    "before the class",
+    "as toilet paper",
+    "when I was gone",
+    "during the lunch",
+    "while I was praying"
+  ];
+  let randomNumber1 = Math.floor(Math.random() * who.length);
+  let randomNumber2 = Math.floor(Math.random() * what.length);
+  let randomNumber3 = Math.floor(Math.random() * when.length);
+
+  return (
+    who[randomNumber1] +
+    " " +
+    what[randomNumber2] +
+    " my homework " +
+    when[randomNumber3]
+  );
 };
